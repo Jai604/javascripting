a805c57c6dcdaff66e8563c58a4ad7b7d135000b
Jai604 <kyle.p.so@gmail.com>
2018-11-18 05:02:19 +0000

    Initial commit of first introductory javascript lessons so far

---
+++ if-statement.js
@@ -0,0 +1,8 @@
+var fruit = 'orange';
+n = fruit.length;
+
+if (n > 5) {
+  console.log("The fruit name has more than five characters.")
+} else {
+  comnsole.log("The fruit name has give characters or less.")
+}
